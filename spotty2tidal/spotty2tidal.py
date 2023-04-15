85015ed607ba46b8f3f68b4c4abde2e68b352b20
hestonhoffman <hestonhoffman@gmail.com>
2023-04-15 10:33:15 -0700

    clean up

--- spotty2tidal/spotty2tidal.py
+++ spotty2tidal/spotty2tidal.py
@@ -3,57 +3,71 @@
 '''
 Turns a Spotify link into a Tidal link.
 '''
-from dotenv import load_dotenv
-import requests, os, re
-import tidalapi
+
+import os
+import re
 import base64
-from pprint import pprint
+from argparse import ArgumentParser
+import requests
+import tidalapi
+from dotenv import load_dotenv
 
 load_dotenv()
 TIDAL_TOKEN = os.getenv('TIDAL_TOKEN')
 T_REFRESH_TOKEN = os.getenv('T_REFRESH_TOKEN')
 SPOTTY_TOKEN = os.getenv('SPOTTY_TOKEN')
 SPOTTY_CLIENT_ID = os.getenv('SPOTTY_CLIENT_ID')
-encoded_secret = base64.b64encode((SPOTTY_CLIENT_ID + ":" + SPOTTY_TOKEN).encode("ascii")).decode("ascii")
+encoded_secret = base64.b64encode(
+        (SPOTTY_CLIENT_ID + ":" + SPOTTY_TOKEN).encode("ascii")
+    ).decode("ascii")
 
 def make_spotty_url(url):
+    '''Create a Spotify endpoint for the track'''
     spotty_url = re.sub('https://open.spotify.com/track/', '', url)
     spotty_url = 'https://api.spotify.com/v1/tracks/' + spotty_url
-    return(spotty_url)
+    return spotty_url
 
-spotty_session = requests.Session()
-token_url = 'https://accounts.spotify.com/api/token'
-headers = {
-    'Authorization': f'Basic {encoded_secret}',
-    'Content-Type': 'application/x-www-form-urlencoded'}
-payload = {'grant_type':'client_credentials'}
-get_token = spotty_session.post(url=token_url, headers=headers, data=payload).json()
-spotify_token = get_token['access_token']
-spotty_session.headers.update({'Authorization': f'Bearer {spotify_token}'})
-example_url = 'https://open.spotify.com/track/15irEKZ9D6FQqLoZ1qJ1Cx'
-url = make_spotty_url(example_url)
-call = spotty_session.get(url)
 
-song = call.json()['name']
-artists = call.json()['artists']
-for item in artists:
-    for entry in item:
-        if entry == 'name':
-            artist = item[entry]
+def get_track_details(user_url):
+    '''Grab track name and artist metadata from Spotify'''
+    spotty_session = requests.Session()
+    token_url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': f'Basic {encoded_secret}',
+        'Content-Type': 'application/x-www-form-urlencoded'}
+    payload = {'grant_type':'client_credentials'}
+    get_token = spotty_session.post(url=token_url, headers=headers, data=payload).json()
+    spotify_token = get_token['access_token']
+    spotty_session.headers.update({'Authorization': f'Bearer {spotify_token}'})
+    url = make_spotty_url(user_url)
+    call = spotty_session.get(url)
+    song = call.json()['name']
+    spotify_artists = []
+    artists = call.json()['artists']
+    for item in artists:
+        for entry in item:
+            if entry == 'name':
+                spotify_artists.append(item[entry])
+    return song, spotify_artists
 
-tsession = tidalapi.Session()
-token_type = 'Bearer'
-access_token = TIDAL_TOKEN
-refresh_token = T_REFRESH_TOKEN
+def match_tidal_track(spotify_song, spotify_artists):
+    '''Use Spotify data to match and find the Tidal URL'''
+    tsession = tidalapi.Session()
+    token_type = 'Bearer'
+    access_token = TIDAL_TOKEN
+    refresh_token = T_REFRESH_TOKEN
 
-tsession.load_oauth_session(token_type, access_token, refresh_token, tsession.expiry_time)
-search_results = tsession.search(song, [tidalapi.media.Track])
-
-for item in search_results['tracks']:
-    if artist == item.artist.name:
-        print(f'https://tidal.com/browse/track/{item.id}')
-
-    #print(item.artist.name)
-    #print(item.id)
-    #print(item.album.name)
-    #print(item.album.id)
+    tsession.load_oauth_session(token_type, access_token, refresh_token, tsession.expiry_time)
+    search_results = tsession.search(spotify_song, [tidalapi.media.Track])
+    for item in search_results['tracks']:
+        print(f"Track name: {item.name}, Artist Name: {item.artist.name}")
+        for tidal_artist in item.artists:
+            if tidal_artist.name in tuple(spotify_artists):
+                return f'https://tidal.com/browse/track/{item.id}'
+    return 'No match found'
+parser = ArgumentParser()
+parser.add_argument('user_url')
+args = parser.parse_args()
+song, artists = get_track_details(args.user_url)
+tidal_url = match_tidal_track(song, artists)
+print(tidal_url)
